0622800574446e266117ddb47e860fb4abeaa8bf
Alexander Repeta <alexander.repeta@gmail.com>
2020-01-18 19:08:08 +0200

    Updates

---
+++ precache-manifest.fb428858270ff032ef5948b2028f11c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9860cf27d83607cb63bbb6d89b4ddb1",
+    "url": "/ghpages-hosting/index.html"
+  },
+  {
+    "revision": "72c56356676af6726cff",
+    "url": "/ghpages-hosting/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "31882d16a605a6c30147",
+    "url": "/ghpages-hosting/static/js/2.9bc9869b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ghpages-hosting/static/js/2.9bc9869b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72c56356676af6726cff",
+    "url": "/ghpages-hosting/static/js/main.2868692c.chunk.js"
+  },
+  {
+    "revision": "7b8ba3978dac2a04b850",
+    "url": "/ghpages-hosting/static/js/runtime-main.66ac6157.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages-hosting/static/media/logo.5d5d9eef.svg"
+  }
+]);
