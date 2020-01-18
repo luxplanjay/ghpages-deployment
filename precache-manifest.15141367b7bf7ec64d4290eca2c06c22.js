e198924464371dbd72043ab5629926b17e0f5d70
Alexander Repeta <alexander.repeta@gmail.com>
2020-01-18 19:31:00 +0200

    Updates

---
+++ precache-manifest.15141367b7bf7ec64d4290eca2c06c22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81edd86dfeff39f96048dfac6f1ffbc1",
+    "url": "/ghpages-deployment/index.html"
+  },
+  {
+    "revision": "72c56356676af6726cff",
+    "url": "/ghpages-deployment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "31882d16a605a6c30147",
+    "url": "/ghpages-deployment/static/js/2.9bc9869b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ghpages-deployment/static/js/2.9bc9869b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72c56356676af6726cff",
+    "url": "/ghpages-deployment/static/js/main.2868692c.chunk.js"
+  },
+  {
+    "revision": "eb68e5f60734bb368705",
+    "url": "/ghpages-deployment/static/js/runtime-main.5b2615d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages-deployment/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ghpages-hosting/precache-manifest.fb428858270ff032ef5948b2028f11c3.js"
+  "/ghpages-deployment/precache-manifest.15141367b7bf7ec64d4290eca2c06c22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ghpages-hosting/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ghpages-deployment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
